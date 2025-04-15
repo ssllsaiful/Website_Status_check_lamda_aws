6c700a6678ddcf00bc46a000bb462e7edd7d953d
Saiful Islam <141049447+ssllsaiful@users.noreply.github.com>
2025-04-15 12:48:22 +0600

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -2,42 +2,16 @@ import json
 import boto3
 import requests
 
-# List of your websites
-# websites = [
-#     'https://facebook.com',
-#     'https://rajneete.com',
-#     'https://bhive.ltd',
-#     # Add more websites here...
-# ]
-
 websites = [
-    'https://baahstore.com',
-    'https://beta.baahstore.com',
-    'https://bdgen24.com',
-    'https://adminnews90.bdnextgen.com',
-    'https://bdnextgen.com',
-    'https://beta.bhive.ltd',
-    'https://betabl.bhive.ltd',
-    'https://bhive.ltd',
-    'https://bladmin.bhive.ltd',
-    'https://multi.bhive.ltd',
-    'https://beta.deshmafood.com',
-    'https://deshmafood.com',
-    'https://funfurti.com',
-    'https://atlas.ideahubbd.com',
-    'https://atlascms.ideahubbd.com',
-    'https://bajp.ideahubbd.com',
-    'https://bdgen24.ideahubbd.com',
-    'https://ideahubbd.com',
-    'https://beta.linstargroup.com',
-    'https://rajneete.com',
-    'https://stratagemhub.com',
-    'https://beta.swadeshionline.shop',
+    'https://youtube.com',
+    'https://facebook.com',
+    'https://ajkerpatrika.com'
+
 ]
 
 
 # SNS configuration
-SNS_TOPIC_ARN = 'arn:aws:sns:us-west-1:067939645276:WebsiteStatusAlerts'  # Replace with your SNS Topic ARN
+SNS_TOPIC_ARN = 'arn:aws:sns:us-west-1:07639645276:WebsiteStatusAlerts'  # Replace with your SNS Topic ARN
 
 def notify_status(down_sites):
     sns_client = boto3.client('sns')
